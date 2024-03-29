4dcadb5f300ad292e198ae1cf1fe5099a9a9e720
Svendpai <antonvibaek@gmail.com>
2024-03-29 10:31:58 +0100

    add function for getting the longest common subsequence

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -148,6 +148,32 @@ def get_token_levenshtein_distance(s1, s2):
 
 
 @timer
+def get_longest_common_subsequence(s1: str, s2: str) -> str:
+    """Get the longest common subsequence of two strings."""
+    m = len(s1)
+    n = len(s2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    i, j = m, n
+    lcs = []
+    while i > 0 and j > 0:
+        if s1[i - 1] == s2[j - 1]:
+            lcs.append(s1[i - 1])
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+    return "".join(reversed(lcs))
+
+
+@timer
 def get_jaccard_similarity(s1: str, s2: str):
     a = set(s1.split())
     b = set(s2.split())
