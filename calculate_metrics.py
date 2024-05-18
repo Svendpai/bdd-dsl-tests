d41611985d274c43b3a48ed560c9cdfde368c76f
Svendpai <antonvibaek@gmail.com>
2024-05-18 23:45:20 +0200

    chore: cleaned up tests + add quantitative tests + new DSL extractor

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -1,25 +1,29 @@
 import json
 from typing import Dict, Any
-import tiktoken
-import numpy as np
-from numpy.linalg import norm
-from sklearn.feature_extraction.text import TfidfVectorizer
+# import tiktoken
+# import numpy as np
+# from numpy.linalg import norm
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 import os
 import shutil
 import subprocess
 import Levenshtein
+import argparse
+from jarowinkler import *
+import difflib
 
-from gensim.models import Word2Vec
-from nltk.tokenize import sent_tokenize, word_tokenize
+# from gensim.models import Word2Vec
+# from nltk.tokenize import sent_tokenize, word_tokenize
 
-import nltk
-nltk.download('punkt')
+# import nltk
+# nltk.download('punkt')
 
 
-ENCODING = tiktoken.get_encoding("cl100k_base")
+# ENCODING = tiktoken.get_encoding("cl100k_base")
 COMPILER_PATH = "./compiler/compiler.jar"
 WIDGETS_PATH = "./base/widgets.bdd"
-DSL_AST_EXTRACTOR_PATH = os.path.join(".","compiler", "DSL_AST_Extractor.jar")
+DSL_AST_EXTRACTOR_PATH = os.path.join(".","compiler", "DSL_AST_Extractor_3.jar")
 
 # Create wrapper for measuring the time it takes to run the function
 def timer(func):
@@ -52,39 +56,6 @@ def save_json(data: Dict[str, Any], output_path: str) -> None:
 
 
 @timer
-def get_word2vec_similarity(s1: str, s2: str) -> float:
-    # Tokenize the strings into sentences
-    sentences_s1 = sent_tokenize(s1)
-    sentences_s2 = sent_tokenize(s2)
-
-    # Tokenize each sentence into words and combine
-    sentences = [word_tokenize(sent) for sent in sentences_s1 + sentences_s2]
-
-    # Train a Word2Vec model
-    model = Word2Vec(sentences, min_count=1)
-
-    # Calculate the similarity between the two strings as the average similarity of their words
-    similarities = []
-    for word in word_tokenize(s1):
-        if word in model.wv.key_to_index:  # Use key_to_index instead of vocab
-            similarities.append(np.mean([model.wv.similarity(word, word2) for word2 in word_tokenize(s2) if word2 in model.wv.key_to_index]))
-
-    return np.mean(similarities)
-
-
-@timer
-def get_cosine_similarity(s1: str, s2: str) -> float:
-    A = np.array(ENCODING.encode(s1))
-    B = np.array(ENCODING.encode(s2))
-
-    if A.shape != B.shape:
-        # Resize A and/or B
-        A = np.resize(A, B.shape)
-
-    return np.dot(A, B)/(norm(A)*norm(B))
-
-
-@timer
 def get_tfidf_similarity(s1: str, s2: str) -> float:
     """The TF-IDF similarity is calculated as the cosine similarity between the TF-IDF vectors of the two strings.
     
@@ -97,7 +68,25 @@ def get_tfidf_similarity(s1: str, s2: str) -> float:
     
     float: The TF-IDF similarity between the two strings.
     """
-    # TODO do not remove stop words (stop words are important in this case)
+    vect = TfidfVectorizer(min_df=1)
+    tfidf = vect.fit_transform([s1, s2])
+    pairwise_similarity = tfidf * tfidf.T
+    return pairwise_similarity.toarray()[0, 1]
+
+
+@timer
+def get_tfidf_stopwords_similarity(s1: str, s2: str) -> float:
+    """The TF-IDF similarity is calculated as the cosine similarity between the TF-IDF vectors of the two strings.
+    
+    Parameters:
+    
+    s1 (str): The first string.
+    s2 (str): The second string.
+    
+    Returns:
+    
+    float: The TF-IDF similarity between the two strings.
+    """
     vect = TfidfVectorizer(min_df=1, stop_words="english")
     tfidf = vect.fit_transform([s1, s2])
     pairwise_similarity = tfidf * tfidf.T
@@ -148,29 +137,11 @@ def get_token_levenshtein_distance(s1, s2):
 
 
 @timer
-def get_longest_common_subsequence(s1: str, s2: str) -> str:
+def get_longest_common_subsequence(s1: str, s2: str) -> int:
     """Get the longest common subsequence of two strings."""
-    m = len(s1)
-    n = len(s2)
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if s1[i - 1] == s2[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-            else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-    i, j = m, n
-    lcs = []
-    while i > 0 and j > 0:
-        if s1[i - 1] == s2[j - 1]:
-            lcs.append(s1[i - 1])
-            i -= 1
-            j -= 1
-        elif dp[i - 1][j] > dp[i][j - 1]:
-            i -= 1
-        else:
-            j -= 1
-    return "".join(reversed(lcs))
+    match = difflib.SequenceMatcher(None, s1, s2)
+    lcs = "".join(s1[x[0]:x[0]+x[2]] for x in match.get_matching_blocks()[:-1])
+    return lcs
 
 
 @timer
@@ -179,6 +150,20 @@ def get_jaccard_similarity(s1: str, s2: str):
     b = set(s2.split())
     c = a.intersection(b)
     return float(len(c)) / (len(a) + len(b) - len(c))
+
+
+@timer
+def get_jaro_winkler_similarity(s1: str, s2: str) -> float:
+    return jarowinkler_similarity(s1, s2)
+
+
+@timer
+def get_bag_of_words_similarity(s1: str, s2: str) -> float:
+    """Calculate the similarity between two strings based on the bag of words model."""
+    vectorizer = CountVectorizer().fit_transform([s1, s2])
+    vectors = vectorizer.toarray()
+    similarity = cosine_similarity(vectors)
+    return similarity[0][1]
 
 
 @timer
@@ -193,60 +178,53 @@ def get_similarity_score(possible_solution_path: str, generated_solution_path: s
     possible_solution = load_text_file(possible_solution_path)
     generated_solution = load_text_file(generated_solution_path)
 
-    # possible_solution_ast = get_ast(possible_solution_path)
-    # generated_solution_ast = get_ast(generated_solution_path)
-    # ast_tfidf_similarity = get_tfidf_similarity(possible_solution_ast, generated_solution_ast)
-    # ast_levenstein_distance = get_levenshtein_distance(possible_solution_ast, generated_solution_ast)
-    # ast_cosine_similarity = get_cosine_similarity(possible_solution_ast, generated_solution_ast)
-    # ast_word2vec_similarity = get_word2vec_similarity(possible_solution_ast, generated_solution_ast)
+    possible_solution_ast = get_ast(possible_solution_path)
+    generated_solution_ast = get_ast(generated_solution_path)
+    print(possible_solution_ast)
 
-    cosine_similarity = get_cosine_similarity(possible_solution, generated_solution)
-    levenshtein_distance = get_levenshtein_distance(possible_solution, generated_solution)
+    ast_tfidf_similarity = get_tfidf_similarity(possible_solution_ast, generated_solution_ast)
+    ast_tfidf_stopwords_similarity = get_tfidf_stopwords_similarity(possible_solution_ast, generated_solution_ast)
+    ast_levenstein_distance = get_levenshtein_distance(possible_solution_ast, generated_solution_ast)
+    ast_jaccard_similarity = get_jaccard_similarity(possible_solution_ast, generated_solution_ast)
+    ast_levenshtein_token_distance = get_token_levenshtein_distance(possible_solution_ast, generated_solution_ast)
+    ast_longest_common_subsequence = get_longest_common_subsequence(possible_solution_ast, generated_solution_ast)
+    ast_length_of_longest_common_subsequence = len(ast_longest_common_subsequence) / min(len(possible_solution_ast), len(generated_solution_ast))
+    ast_jaro_winkler_similarity = get_jaro_winkler_similarity(possible_solution_ast, generated_solution_ast)
+    ast_bag_of_words_similarity = get_bag_of_words_similarity(possible_solution_ast, generated_solution_ast)
+
     tfidf_similarity = get_tfidf_similarity(possible_solution, generated_solution)
+    tfidf_stopwords_similarity = get_tfidf_stopwords_similarity(possible_solution, generated_solution)
+    levenshtein_distance = get_levenshtein_distance(possible_solution, generated_solution)
     jaccard_similarity = get_jaccard_similarity(possible_solution, generated_solution)
     levenshtein_token_distance = get_token_levenshtein_distance(possible_solution, generated_solution)
     longest_common_subsequence = get_longest_common_subsequence(possible_solution, generated_solution)
     length_of_longest_common_subsequence = len(longest_common_subsequence) / min(len(possible_solution), len(generated_solution))
-    # word2vec_similarity = get_word2vec_similarity(possible_solution, generated_solution)
+    jaro_winkler_similarity = get_jaro_winkler_similarity(possible_solution, generated_solution)
+    bag_of_words_similarity = get_bag_of_words_similarity(possible_solution, generated_solution)
 
     similarity = {
-        "cosine_similarity": float(f"{cosine_similarity:.4f}"),
         "levenshtein_distance": levenshtein_distance,
         "tfidf_similarity": float(f"{tfidf_similarity:.4f}"),
+        "tfidf_stopwords_similarity": float(f"{tfidf_stopwords_similarity:.4f}"),
         "jaccard_similarity": float(f"{jaccard_similarity:.4f}"),
         "levenshtein_distance_token": levenshtein_token_distance,
         "longest_common_subsequence": float(f"{length_of_longest_common_subsequence:.4f}"),
-        # "word2vec_similarity": f"{word2vec_similarity:.4f}",
-        # "ast_tfidf_similarity": f"{ast_tfidf_similarity:.4f}",
-        # "ast_levenstein_distance": ast_levenstein_distance,
-        # "ast_cosine_similarity": f"{ast_cosine_similarity:.4f}",
-        # "ast_word2vec_similarity": f"{ast_word2vec_similarity:.4f}",
-
+        "jaro_winkler_similarity": float(f"{jaro_winkler_similarity:.4f}"),
+        "bag_of_words_similarity": float(f"{bag_of_words_similarity:.4f}"),
+        "ast_tfidf_similarity": float(f"{ast_tfidf_similarity:.4f}"),
+        "ast_levenstein_distance": ast_levenstein_distance,
+        "ast_jaccard_similarity": float(f"{ast_jaccard_similarity:.4f}"),
+        "ast_longest_common_subsequence": float(f"{ast_length_of_longest_common_subsequence:.4f}"),
+        "ast_levenshtein_token_distance": ast_levenshtein_token_distance,
+        "ast_jaro_winkler_similarity": float(f"{ast_jaro_winkler_similarity:.4f}"),
+        "ast_bag_of_words_similarity": float(f"{ast_bag_of_words_similarity:.4f}"),
+        "ast_tfidf_stopwords_similarity": float(f"{ast_tfidf_stopwords_similarity:.4f}")
     }
 
     return similarity
 
 
-def get_score_of_generated_data(possible_solution_folder_path: str, generated_folder_path: str) -> str:
-    """Get the similarity score of each generated .bdd file compared to the possible solution .bdd files."""
-    scores = {}
-    for possible_solution in os.listdir(possible_solution_folder_path):
-        for generated in os.listdir(generated_folder_path):
-            print(generated)
-            if not possible_solution.endswith(".bdd"): continue
-            if not generated.endswith(".bdd"): continue
-            if possible_solution != generated.split("_")[0] + ".bdd": continue
-            
-            scores[generated] = get_similarity_score(
-                os.path.join(possible_solution_folder_path, possible_solution), 
-                os.path.join(generated_folder_path, generated))
-            
-            scores[generated]["compiler_log"] = compile_model(os.path.join(generated_folder_path, generated))
-
-    return json.dumps(scores, sort_keys=True, indent=4, ensure_ascii=False)
-
-
-def get_score_of_generated_data_2(ground_truth_bdd_path: str, generated_folder_path: str) -> str:
+def get_score_of_generated_data(ground_truth_bdd_path: str, generated_folder_path: str) -> str:
 
     scores = {}
     for version in os.listdir(generated_folder_path):
@@ -286,12 +264,14 @@ def clean_llm_ouput(dsl_file_path: str) -> str:
         cleaned_content = ''.join(lines[start_idx:end_idx])
     elif start_idx is not None:
         cleaned_content = ''.join(lines[start_idx:])  # If there's no ending "```", take everything after the starting one
+    else:
+        cleaned_content = ''.join(lines)
 
     return cleaned_content
 
 
 @timer
-def compile_model(dsl_file_path: str, compiler_path: str = COMPILER_PATH, widgets_path: str = WIDGETS_PATH) -> list[str]:
+def compile_model(dsl_file_path: str, compiler_path: str = COMPILER_PATH, widgets_path: str = WIDGETS_PATH, clean_output: bool = True) -> list[str]:
     """Compile the given .bdd file using the BDD-DSL compiler and return the output as a list of strings."""
     temp_dir = "temp"
     temp_dir_path = os.path.join(os.getcwd(), temp_dir)
@@ -300,12 +280,16 @@ def compile_model(dsl_file_path: str, compiler_path: str = COMPILER_PATH, widget
     shutil.copy(widgets_path, temp_dir_path)
     shutil.copy(compiler_path, temp_dir_path)
 
-    cleaned_content = clean_llm_ouput(dsl_file_path)
+    new_dsl_file_path = os.path.join(temp_dir_path, os.path.basename(dsl_file_path))
 
-    cleaned_file_path = os.path.join(temp_dir_path, os.path.basename(dsl_file_path))
-    with open(cleaned_file_path, 'w') as cleaned_file:
-        cleaned_file.write(cleaned_content)
-    
+    if clean_output:
+        cleaned_content = clean_llm_ouput(dsl_file_path)
+        
+        with open(new_dsl_file_path, 'w') as cleaned_file:
+            cleaned_file.write(cleaned_content)
+    else:
+        shutil.copy(dsl_file_path, new_dsl_file_path)
+
     new_compiler_path = os.path.join(temp_dir_path, os.path.split(compiler_path)[1])
 
     completed_process = subprocess.run(["java", "-jar", new_compiler_path, os.path.join(".", temp_dir)], stdout=subprocess.PIPE, text=True)
@@ -320,18 +304,17 @@ def compile_model(dsl_file_path: str, compiler_path: str = COMPILER_PATH, widget
     return output
 
 
-def main():
-    # possible_solution_path = "./possible_solutions"
-    ground_truth = "./possible_solutions/eCommerce.bdd"
-    generated_path = "./generated"
-
-    # scores = get_score_of_generated_data(possible_solution_path, generated_path)
-    # save_json(scores, "similarity_scores.json")
-
-    scores = get_score_of_generated_data_2(ground_truth, generated_path)
+def main(ground_truth, generated_path, output_path = "output.json"):
+    scores = get_score_of_generated_data(ground_truth, generated_path)
     print(scores)
-    save_json(scores, "similarity_scores_2.json")
+    save_json(scores, output_path)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Calculate metrics.')
+    parser.add_argument('--ground-truth', type=str, required=True, help='Path to the ground truth file.')
+    parser.add_argument('--folder-path', type=str, required=True, help='Path to the folder containing generated data.')
+    parser.add_argument('--output', type=str, required=False, help='Path to the output file.')
+    args = parser.parse_args()
+
+    main(args.ground_truth, args.folder_path, args.output)
